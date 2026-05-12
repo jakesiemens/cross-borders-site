74bc44af7470abca4e2d293852d00ac50335e889
Jake Siemens <jakesiemens@users.noreply.github.com>
2026-05-11 21:00:42 -0300

    Update John and Susie Neufeld bio and image

--- draft/js/data.js
+++ draft/js/data.js
@@ -258,8 +258,8 @@ const missionaryFamilies = [
     id: "neufeld",
     name: "John and Susie Neufeld",
     role: "Core Team",
-    image: "./images/Missionaries/john-susie.jpg",
-    bio: "Serving as missionaries with Cross Borders Ministries."
+    image: "./images/Missionaries/John and Susie Nefeld Family.jpeg",
+    bio: "My wife, Susie, and I along with our 4 children, Israel 13, Chloe 11, Destiny 6 and Carter 3, are new to the field. Originally from Ontario, Canada, we are currently based in Chiang Mai, Thailand.<br><br>We are excited as we embark on this new journey serving the people of Thailand and surrounding countries.<br><br>We are blessed to meet local believers and excited to walk alongside them as they learn more about the Jesus way.<br><br>Our hearts are saddened by the growing number of orphaned children due to neglect and neighbouring war. We are thankful to be able to reach out to orphanages and help them with food provisions and meet practical needs.<br><br>Our mission is to share the love and power of Jesus with the Thai community by living with them in the daily grind. Teaching and discipling new believers. And getting God’s word into the hands of hungry Christians. To the glory of God and the furtherance of His Kingdom.<br><br>John and Susie Neufeld"
   }
 ];
 
